9c16e18d104c025dfd9d470fe7adf45a7ee78331
SimoVisc <simonaviscardivs@gmail.com>
2022-10-26 11:30:39 +0200

    finisce l'esercizio

--- jsnack-2/main.js
+++ jsnack-2/main.js
@@ -3,8 +3,8 @@ const Parola1= prompt("inserisci una parola")
 const Parola2= prompt("inserisci un'altra parola")
 // Il software stampa prima la parola più corta, poi la parola più lunga
 
- if (Parola1< Parola2){
-        console.log(Parola2,Parola1)
- } else{
+ if (Parola1.lenth< Parola2.lenth){
         console.log(Parola1,Parola2)
+ } else{
+        console.log(Parola2,Parola1)
  }
