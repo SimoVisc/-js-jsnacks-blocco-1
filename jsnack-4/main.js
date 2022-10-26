ef89615d63ed5f664d88e4dd432f70ae27a84f08
SimoVisc <simonaviscardivs@gmail.com>
2022-10-26 12:09:50 +0200

    Cambia il nome dell'utente da "name2" a "username"

--- jsnack-4/main.js
+++ jsnack-4/main.js
@@ -1,10 +1,10 @@
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby
 const guests=["mario","filippo","luca","simona"];
 //  chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa
-const name2= prompt("inserisci il tuo nome");
+const username= prompt("inserisci il tuo nome");
 let guestname= false;
 for(let i=0; i<guests.length; i++ ){
-   if(name2=== guests[i]){
+   if(username=== guests[i]){
       guestname=true;
    }
 }
