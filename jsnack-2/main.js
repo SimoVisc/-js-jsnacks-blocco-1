f74793f7c53dd56ff78dece139ab7228d3257aab
SimoVisc <simonaviscardivs@gmail.com>
2022-10-26 11:01:26 +0200

    aggiunge la struttura di base  e il comando per inserire due parole

---
+++ jsnack-2/main.js
@@ -0,0 +1,5 @@
+// L’utente inserisce due parole in successione, con due prompt
+const Parola1= promt("inserisci una parola")
+const Parola2= promt("inserisci un'altra parola")
+// Il software stampa prima la parola più corta, poi la parola più lunga
+
