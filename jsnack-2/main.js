4877bbea4c091ec1cb694717d6e045457f0c2ea4
SimoVisc <simonaviscardivs@gmail.com>
2022-10-26 11:16:52 +0200

    Aggiunge il comando per stampare prima la parola più corta

--- jsnack-2/main.js
+++ jsnack-2/main.js
@@ -1,4 +1,10 @@
 // L’utente inserisce due parole in successione, con due prompt
-let Parola1= prompt("inserisci una parola")
-let Parola2= prompt("inserisci un'altra parola")
+const Parola1= prompt("inserisci una parola")
+const Parola2= prompt("inserisci un'altra parola")
 // Il software stampa prima la parola più corta, poi la parola più lunga
+
+ if (Parola1< Parola2){
+        console.log(Parola2,Parola1)
+ } else{
+        console.log(Parola1,Parola2)
+ }
