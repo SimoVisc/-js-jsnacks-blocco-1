c3f1136d5c602c4d8e7d12e4f237f875aff7222e
SimoVisc <simonaviscardivs@gmail.com>
2022-10-26 12:36:46 +0200

    aggiunge un numero dispari all'inerno dell'arrey list

---
+++ jsnack-5/main.js
@@ -0,0 +1,14 @@
+// Crea un array vuoto
+const list=[]
+// Chiedi per 6 volte all’utente di inserire un numero
+let amount= 0
+for(let i=0; i<=3; i++){
+   const number= Number(prompt("inserisci un numero"))
+   amount += number
+}
+// se è dispari inseriscilo nell’array
+if(number % 2===0){
+   console.log(i)
+}else{
+   console.log(list)
+}
