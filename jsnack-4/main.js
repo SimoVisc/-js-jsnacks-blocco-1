9da4a156d3bd88e72371ad5a9130a0a6c9bd1c16
SimoVisc <simonaviscardivs@gmail.com>
2022-10-26 11:59:34 +0200

    aggiunge lista invitati

---
+++ jsnack-4/main.js
@@ -0,0 +1,3 @@
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby
+const guests=["mario","filippo","luca","simona"]
+//  chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
