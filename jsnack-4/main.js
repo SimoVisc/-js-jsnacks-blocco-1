282dd44cbafb307a3ab878504e3b7310d462a358
SimoVisc <simonaviscardivs@gmail.com>
2022-10-26 12:07:49 +0200

    aggiunge il comando per chiedere il nome all'utente e comunicare se può partecipare alla festa

--- jsnack-4/main.js
+++ jsnack-4/main.js
@@ -1,3 +1,15 @@
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby
-const guests=["mario","filippo","luca","simona"]
-//  chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+const guests=["mario","filippo","luca","simona"];
+//  chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa
+const name2= prompt("inserisci il tuo nome");
+let guestname= false;
+for(let i=0; i<guests.length; i++ ){
+   if(name2=== guests[i]){
+      guestname=true;
+   }
+}
+if(guestname){
+   alert("Benvenuto alla festa")
+}else{
+   alert("accesso negato")
+}
