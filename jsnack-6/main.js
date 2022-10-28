7b756d5830a99141fbd6045aa5d729de7f186b08
SimoVisc <simonaviscardivs@gmail.com>
2022-10-28 10:52:36 +0200

    aggiunge la somma dei numeri dispari

--- jsnack-6/main.js
+++ jsnack-6/main.js
@@ -1,6 +1,14 @@
 "use strict"
 // Crea un array di numeri interi 
-const listNumbers=["1","2","3","4","5","6","7","8","9","10"]
-
+const listNumbers=[1,2,3,4,5,6,7,8,9,10]
 
 // fai la somma di tutti gli elementi  che sono in posizione dispari
+
+let oddNumber= 0;
+
+for(let i=0; i<listNumbers.length; i++){
+   if(i %2 ==1){
+      oddNumber= oddNumber+ listNumbers[i];
+   }
+}
+console.log(oddNumber)
