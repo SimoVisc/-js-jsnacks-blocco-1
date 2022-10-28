c1a8a930ea50569dc1b3f2244920b51aa683f6bb
SimoVisc <simonaviscardivs@gmail.com>
2022-10-28 10:23:05 +0200

    Aggiunge un arrey di numeri interi

---
+++ jsnack-6/main.js
@@ -0,0 +1,6 @@
+"use strict"
+// Crea un array di numeri interi 
+const listNumbers=["1","2","3","4","5","6","7","8","9","10"]
+
+
+// fai la somma di tutti gli elementi  che sono in posizione dispari
